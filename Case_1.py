6b26cb5b42c16368a73499078ad5e849c3e488da
Duong Anh <Duonganh7805@gmail.com>
2025-05-04 21:33:55 +0700

    Nến búa

---
+++ Case_1.py
@@ -0,0 +1,13 @@
+# Nến búa 
+""" Là nến mà trong 1 xu hướng giảm liên tiếp của thị trường, xuất hiện một nến bất thường,
+tức là giá đóng cửa gần bằng giá mở cửa (Cây nến hôm đấy phải ), bóng nến dưới dài 
+"""
+import Phase_2 
+
+"""Kiểm tra xu hướng giảm liên tục trong n ngày gần nhất"""
+def DownTrend(): 
+
+"""Kiểm tra xem nến hiện tại có phải nến búa không""" 
+def Hammer(candle): 
+    
+
