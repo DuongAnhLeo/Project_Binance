0846c9932ddd9fa06f28bc25607db0b9f57be737
Duong Anh <Duonganh7805@gmail.com>
2025-05-09 02:17:39 +0700

    Xu hướng của thị trường

---
+++ Trend.py
@@ -0,0 +1,28 @@
+def DownTrend(Candles_Map, index, window=3, allow_break=1): 
+    if index <= window: 
+        return False
+
+    breaks = 0
+    for i in range(index - window + 1, index + 1): 
+        prev_candles = Candles_Map[i - 1] 
+        curr_candles = Candles_Map[i] 
+        if prev_candles.get_close_price() <= curr_candles.get_close_price(): 
+            breaks += 1
+            if breaks > allow_break: 
+                return False
+    return True  
+
+
+def UpTrend(Candles_Map, index, window=3, allow_break=1): 
+    if index <= window: 
+        return False
+
+    breaks = 0
+    for i in range(index - window + 1, index + 1): 
+        prev_candles = Candles_Map[i - 1] 
+        curr_candles = Candles_Map[i] 
+        if prev_candles.get_close_price() >= curr_candles.get_close_price(): 
+            breaks += 1
+            if breaks > allow_break: 
+                return False
+    return True  
