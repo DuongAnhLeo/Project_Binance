7b162d6dc8fb21e86b7e151131c5722b8117d9f5
Duong Anh <Duonganh7805@gmail.com>
2025-05-09 16:10:33 +0700

    Hoàn chỉnh điều kiện của mẫu hình Mây đen bao phủ

--- Case_2.py
+++ Case_2.py
@@ -1,3 +1,4 @@
+#Case này là xu hướng tăng của thị trường
 """Nến người treo cổ: nếu xuất hiện sau xu hướng tăng của thị trường, mẫu
  hình có thể đảo chiều
 """
@@ -14,3 +15,26 @@ def HangingMan(candle):
     
     return low_shadow >= 1.5 * body and up_shadow <= body*0.5 
     #bóng dưới dài gấp 2 lần thân nến, bóng trên thấp
+
+
+"""Mẫu hình Mây đen bao phủ
+
+Là nến xuất hiện sau xu hướng tăng củ thị trường, gồm có 2 nến: nến thứ nhất là nến tăng,
+nến thứ 2 là nến giảm bao phủ hơn 1/2 nến thứ 1
+""" 
+
+def DarkCloudCover(candle1, candle2):
+    # Nến 1 là tăng (thân xanh)
+    if candle1.get_close_price() <= candle1.get_open_price():
+        return False
+
+    # Nến 2 là giảm (thân đỏ)
+    if candle2.get_close_price() >= candle2.get_open_price():
+        return False
+    
+    #Mức 1/2 thân nến thứ 1
+    midpoint_candle1 = (candle1.get_open_price() + candle1.get_close_price()) / 2
+    
+    # Điều kiện Mây Đen Bao Phủ
+    return (candle2.get_open_price() > candle1.get_close_price() and
+            candle2.get_close_price() < midpoint_candle1)
