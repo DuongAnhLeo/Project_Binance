794734c410b2e82504fe8c6e3fcc947d293ec236
Duong Anh <Duonganh7805@gmail.com>
2025-05-10 21:00:22 +0700

    Sửa lại điều kiện của Mây đen bao phủ

--- Case_2.py
+++ Case_2.py
@@ -1,4 +1,5 @@
 #Case này là xu hướng tăng của thị trường
+
 """Nến người treo cổ: nếu xuất hiện sau xu hướng tăng của thị trường, mẫu
  hình có thể đảo chiều
 """
@@ -22,13 +23,13 @@ def HangingMan(candle):
 Là nến xuất hiện sau xu hướng tăng củ thị trường, gồm có 2 nến: nến thứ nhất là nến tăng,
 nến thứ 2 là nến giảm bao phủ hơn 1/2 nến thứ 1
 """ 
-
+#Mẫu hình mây đen bao phủ
 def DarkCloudCover(candle1, candle2):
     # Nến 1 là tăng (thân xanh)
     if candle1.get_close_price() <= candle1.get_open_price():
-        return False
+        return False 
 
-    # Nến 2 là giảm (thân đỏ)
+    # Nến 2 là giảm (thân đỏ) 
     if candle2.get_close_price() >= candle2.get_open_price():
         return False
     
@@ -37,4 +38,22 @@ def DarkCloudCover(candle1, candle2):
     
     # Điều kiện Mây Đen Bao Phủ
     return (candle2.get_open_price() > candle1.get_close_price() and
-            candle2.get_close_price() < midpoint_candle1)
+            candle2.get_close_price() < midpoint_candle1 and 
+            candle2.get_close_price() > candle1.get_open_price())
+
+"""Mẫu hình Nhấn Chìm Giảm: Là mẫu hình xuất hiện trong xu hướng tăng của thị trường,
+báo hiệu dấu hiệu tiêu cực. Gồm có 2 nến: Nến 1 là nến tăng, Nến 2 là 1 nến giảm nuốt trọn
+nến 1""" 
+#Mẫu hình Nhấn chìm giảm
+def BearishEngulfing(candle1, candle2):
+    # Nến 1 là tăng (thân xanh)
+    if candle1.get_close_price() <= candle1.get_open_price():
+        return False
+
+    # Nến 2 là giảm (thân đỏ)
+    if candle2.get_close_price() >= candle2.get_open_price():
+        return False
+
+    # Điều kiện thân nến 2 bao trùm toàn bộ thân nến 1
+    return (candle2.get_open_price() >= candle1.get_close_price() and
+            candle2.get_close_price() <= candle1.get_open_price())
